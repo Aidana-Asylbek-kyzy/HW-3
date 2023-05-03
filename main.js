ab829eb63f67c2c26a2813e947de381b9c3fc349
Aidana-Asylbek-kyzy <aidanaasylbekkyzy12@gmail.com>
2023-05-03 17:28:19 +0600

    сделала дз, доп пока не сделала. если сделаю доп то в изменении напишу что сделала доп.

---
+++ main.js
@@ -0,0 +1,36 @@
+///////////////////////
+
+
+// function compare(num1, num2) {
+//     if(num1 < num2) {
+//         console.log('первое меньше чем второе число');
+//     }else if(num2 < num1) {
+//         console.log('второе число меньше чем первое число');
+//     }else console.log('оба числа равны');
+// }
+
+
+/////////////////////////
+
+
+// console.log(compare(5,5));
+
+// function degree (num, degree) {
+    // return num**degree;
+// }
+
+// console.log(degree(3, 2));
+
+
+///////////////////////////
+
+
+// var arr = [1,2,3,4,5];
+// function arraySum(array){
+//     var sum = 0;
+//     for(var i = 0; i < array.length; i++){
+//         sum += array[i];
+//     }
+//     console.log(sum);
+// }
+// arraySum(arr);
