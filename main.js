12a67377d870786e700a2031bc4a0ab714a9019e
Aidana-Asylbek-kyzy <aidanaasylbekkyzy12@gmail.com>
2023-05-03 19:34:36 +0600

    fixed

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-///////////////////////
-
-
 // function compare(num1, num2) {
 //     if(num1 < num2) {
 //         console.log('первое меньше чем второе число');
@@ -8,22 +5,12 @@
 //         console.log('второе число меньше чем первое число');
 //     }else console.log('оба числа равны');
 // }
-
-
-/////////////////////////
-
-
-// console.log(compare(5,5));
+// console.log(compare(6,1));
 
 // function degree (num, degree) {
-    // return num**degree;
+//     return num**degree;
 // }
-
-// console.log(degree(3, 2));
-
-
-///////////////////////////
-
+// console.log(degree(2,3));
 
 // var arr = [1,2,3,4,5];
 // function arraySum(array){
@@ -34,3 +21,25 @@
 //     console.log(sum);
 // }
 // arraySum(arr);
+
+//дополнительные
+
+// let user1 = {
+//     phoneNumber: '+996708122004',
+//     sms: 'str',
+//     sendTransfer: function(phoneNum, str)  {
+//         console.log(phoneNum, str);
+//     }
+// };
+// console.log(user1.sendTransfer('+996708122004', 'привет'));
+
+// let user2 = {
+//     name: "Aidana",
+//     age: 19,
+//     accountNumber: '+996708122004',
+//     balance: 1000,
+//     sendTransfer: (accountNum, sum) => {
+//         console.log('Номер счета', accountNum, 'Остаток баланса', user2.balance - sum);
+//     }
+// };
+// console.log(user2.sendTransfer('+996708435465', 600));
